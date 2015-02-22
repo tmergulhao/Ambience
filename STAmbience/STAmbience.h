3601b168d3d819f8bf0820897accc9d8ec741bea
Tiago Mergulhão <me@tmergulhao.com>
2015-02-21 23:03:14 -0300

    Initial Commit

---
+++ STAmbience/STAmbience.h
@@ -0,0 +1,19 @@
+//
+//  STAmbience.h
+//  STAmbience
+//
+//  Created by Tiago Mergulhão on 21/02/15.
+//  Copyright (c) 2015 Tiago Mergulhão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STAmbience.
+FOUNDATION_EXPORT double STAmbienceVersionNumber;
+
+//! Project version string for STAmbience.
+FOUNDATION_EXPORT const unsigned char STAmbienceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STAmbience/PublicHeader.h>
+
+
