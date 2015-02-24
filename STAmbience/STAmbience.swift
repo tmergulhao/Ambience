a5cec2974ad6c5d5ccc283cbda078b1edfc44bea
Tiago Mergulhão <me@tmergulhao.com>
2015-02-24 14:12:13 -0300

    Dynamic assignable threasholds
    Using sets and hash values the system got smarter and more funcional.
    Now the user can override the standard constraints.

--- STAmbience/STAmbience.swift
+++ STAmbience/STAmbience.swift
@@ -8,109 +8,159 @@
 
 import UIKit
 
-//	TODO: Dynamic thresholds
-
-public typealias STBrightness = CGFloat
-
-private func inBetween<T : Comparable>(value : T, border : (lower : T, upper : T)) -> Bool {
-	return value >= border.lower && value <= border.upper
+public func ==(lhs: AmbienceConstraint, rhs: AmbienceConstraint) -> Bool {
+	return lhs.hashValue == rhs.hashValue
+}
+public func <(lhs: AmbienceConstraint, rhs: AmbienceConstraint) -> Bool {
+	return lhs.hashValue < rhs.hashValue
 }
 
-public enum AmbienceState : Printable {
-	case Invert, Regular, Contrast
+public typealias Brightness = CGFloat
+
+public enum AmbienceConstraint : Printable, Hashable, Comparable {
+	case Invert(upper : Brightness)
+	case Regular(lower : Brightness, upper : Brightness)
+	case Contrast(lower : Brightness)
+	
+	public var simpleDescription : String {
+		switch self {
+		case .Invert(let upper): return "invert"
+		case .Regular(let lower, let upper): return "regular"
+		case .Contrast(let lower): return "contrast"
+		}
+	}
 	
 	public var description : String {
 		switch self {
-		case .Invert: return "invert"
-		case .Regular: return "regular"
-		case .Contrast: return "contrast"
+		case .Invert(let upper): return "invert \(0.0, upper)"
+		case .Regular(let lower, let upper): return "regular \(lower, upper)"
+		case .Contrast(let lower): return "contrast \(lower, 1.0)"
 		}
 	}
 	
-	private var range : (STBrightness, STBrightness) {
+	public var hashValue : Int {
 		switch self {
-		case .Invert: return (0.0, 0.05)
-		case .Regular: return (0.1, 0.9)
-		case .Contrast: return (0.95, 1)
+		case .Invert: return simpleDescription.hashValue
+		case .Regular: return simpleDescription.hashValue
+		case .Contrast: return simpleDescription.hashValue
 		}
 	}
 	
-	private func inBrightnessRange (value : STBrightness) -> Bool {
-		return inBetween(value, self.range)
+	private var range : (lower : Brightness, upper : Brightness) {
+		switch self {
+		case .Invert(let upper): return (0.0, upper)
+		case .Regular(let lower, let upper): return (lower, upper)
+		case .Contrast(let lower): return (lower, 1)
+		}
 	}
 	
-	private static func acceptableStates (forBrightness value : STBrightness) -> Set<AmbienceState> {
-		
-		var acceptableStates : Set<AmbienceState> = []
-		
-		if AmbienceState.Regular.inBrightnessRange(value) {
-			acceptableStates.insert(.Regular)
-		}
-		
-		if AmbienceState.Invert.inBrightnessRange(value) {
-			acceptableStates.insert(.Invert)
-		}
-		if AmbienceState.Contrast.inBrightnessRange(value) {
-			acceptableStates.insert(.Contrast)
-		}
-		
-		return acceptableStates
+	private func inBrightnessRange (value : Brightness) -> Bool {
+		return value >= range.lower && value <= range.upper
 	}
 }
 
-public extension UIViewController {
-	final func notifyAmbience () {
-		STAmbience.viewControllers.insert(self)
-		ambience (didChangeFrom: STAmbience.lastState, to: STAmbience.state)
-	}
-	
-	final func stopAmbience () {
-		STAmbience.viewControllers.remove(self)
-	}
-	
-	func ambience (didChangeFrom lastState : AmbienceState?, to currentState : AmbienceState) {}
+public class AmbienceViewController : UIViewController {
+	func ambience (didChangeFrom previousState : AmbienceConstraint?, to currentState : AmbienceConstraint?) {}
 }
 
-private class STAmbience : NSObject {
+public class STAmbience : NSObject {
 	
-	private static var viewControllers : Set<UIViewController> = [] {
+	// return false on error
+	internal class func insert (viewController : AmbienceViewController) -> Bool {
+		if !viewControllers.contains(viewController) {
+			viewControllers.insert(viewController)
+			
+			viewController.ambience(didChangeFrom: previousState, to: currentState)
+			
+			return false
+		}
+		
+		return true
+	}
+	
+	internal class func remove (viewController : AmbienceViewController) -> Bool {
+		if viewControllers.contains(viewController) {
+			viewControllers.remove(viewController)
+			
+			return false
+		}
+		
+		return true
+	}
+	
+	public class func resetConstraint () {
+		constraints = standartConstraints
+	}
+	internal class func insert (newConstraint : AmbienceConstraint) {
+		constraints.insert(newConstraint)
+	}
+	internal class func insert (newConstraints : Set<AmbienceConstraint>) {
+		println(newConstraints)
+		println(constraints)
+		constraints.unionInPlace(newConstraints)
+	}
+	
+	private static let standartConstraints : Set<AmbienceConstraint> = [
+		.Invert(upper: 0.10),
+		.Regular(lower: 0.05, upper: 0.95),
+		.Contrast(lower: 0.90)
+	]
+	
+	private static var constraints  = standartConstraints {
+		didSet {
+			brightnessDidChange(NSNotification(name: "", object: nil))
+		}
+	}
+	
+	private static var viewControllers : Set<AmbienceViewController> = [] {
 		willSet {
 			if viewControllers.isEmpty {
-				checkBrightnessState()
+				brightnessDidChange(NSNotification(name: "", object: nil))
 			}
 		}
 		didSet {
 			if viewControllers.isEmpty {
 				NSNotificationCenter.defaultCenter().removeObserver(self)
 			} else if viewControllers.count == 1 {
-				NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("brightnessDidChange:"), name: UIScreenBrightnessDidChangeNotification, object: nil)
+				NSNotificationCenter.defaultCenter().addObserver(self,
+					selector: Selector("brightnessDidChange:"),
+					name: UIScreenBrightnessDidChangeNotification,
+					object: nil)
 			}
 		}
 	}
 	
-	private static var lastState : AmbienceState?
-	private static var state : AmbienceState = .Regular {
+	private static var previousState : AmbienceConstraint?
+	private static var currentState : AmbienceConstraint? {
 		willSet {
-			lastState = state
+			previousState = currentState
 		}
 		didSet {
 			for viewController in viewControllers {
-				viewController.ambience(didChangeFrom: lastState, to: state)
+				viewController.ambience(didChangeFrom: previousState, to: currentState)
 			}
 		}
 	}
 	
-	private class func checkBrightnessState () {
-		let currentBrightness = UIScreen.mainScreen().brightness
-		let acceptableStates = AmbienceState.acceptableStates(forBrightness: currentBrightness)
-		
-		if let someState = acceptableStates.first where !acceptableStates.contains(state) {
-			state = someState
+	public static var dummyMode : Bool = false
+	public static var dummyBrightness : Brightness = 0.5 {
+		didSet {
+			brightnessDidChange(NSNotification(name: "", object: nil))
 		}
 	}
 	
 	private class func brightnessDidChange (notification : NSNotification) {
-		checkBrightnessState()
+		let currentBrightness : Brightness = dummyMode ? dummyBrightness : UIScreen.mainScreen().brightness
+		
+		let acceptableStates : Set<AmbienceConstraint> = Set(filter(constraints){
+			$0.inBrightnessRange(currentBrightness)
+		})
+		
+		if let someState = currentState where !acceptableStates.contains(someState) {
+			currentState = acceptableStates.first
+		} else if let newState = acceptableStates.first where currentState == nil {
+			currentState = newState
+		}
 	}
 	
 }
